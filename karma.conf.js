5edf044530d94c5f8ac6d46f910a9389002864aa
Mariusz Michalski <supermariusz@gmail.com>
2014-06-08 13:50:36 +0200

    improve(tests): replaced Chrome with faster PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,22 @@
 /* jshint strict: false */
 
-module.exports = function (karma) {
-    karma.set({
+module.exports = function (config) {
+    config.set({
         frameworks: ['jasmine'],
-        plugins: ['karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-phantomjs-launcher'],
-        /**
-         * The list of browsers to launch to test on. This includes only "Firefox" by
-         * default, but other browser names include:
-         * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
-         *
-         * Note that you can also use the executable name of the browser, like "chromium"
-         * or "firefox", but that these vary based on your operating system.
-         */
+        plugins: ['karma-jasmine', 'karma-phantomjs-launcher', 'karma-coverage'],
+
+        reporters: ['progress', 'coverage'],
+
+        preprocessors: {
+            'src/**/*.js': 'coverage'
+        },
+
+        coverageReporter: {
+            type: 'text'
+        },
+
         browsers: [
-            'Chrome'
+            'PhantomJS'
         ]
     });
 };
